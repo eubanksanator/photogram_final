73f9a39edf9daa460f9d151e57c62cae6417f89b
Raghu Betina <rbetina@github.com>
2015-03-04 16:12:54 -0600

    Removes index updates

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,32 +3,8 @@ class Photo < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
-
-  # def owner
-  #   return User.find_by :id => self.owner_id
-  # end
-
   has_many :comments
   has_many :favoritings
 
 end
-
-# Photo should belong to an owner (User)
-# Comment should belong to a commenter (User)
-# Comment should belong to a photo
-# Favoriting should belong to a favorited_by (User)
-# Favoriting should belong to a photo
-
-# User should have many comments (foreign key: commenter_id)
-# User should have many own_photos (foreign_key: owner_id)
-# User should have many favoritings (foreign key: favorited_by_id)
-# Photo should have many comments
-# Photo should have many favoritings
-
-
-
-# Photo should have many fans (Users)
-# User should have many favorite_photos (Photos)
-
-
 
