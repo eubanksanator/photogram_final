fa130dfc9675770b3865cd2cc1f184f9065618ae
Raghu Betina <rbetina@github.com>
2015-03-04 14:31:52 -0600

    Example of non-standard association

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,9 @@
 class Photo < ActiveRecord::Base
+
+  belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
+
+  # def owner
+  #   return User.find_by :id => self.owner_id
+  # end
+
 end
