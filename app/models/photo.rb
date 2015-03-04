e8727a197a66e5d4d444578b2f4a742632be5cee
Raghu Betina <rbetina@github.com>
2015-03-04 14:54:23 -0600

    Adds CarrierWave for image uploads

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
 
+  mount_uploader :image, ImageUploader
+
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
 
   # def owner
@@ -7,3 +9,11 @@ class Photo < ActiveRecord::Base
   # end
 
 end
+
+# Comment should belong to a commenter (User)
+# Favorite should belong to a favorited_by (User)
+# Comment should belong to a photo
+
+# User should have many comments
+# Photo should have many fans (Users)
+# Photo should have many comments
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304200625) do
+ActiveRecord::Schema.define(version: 20150304204056) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150304200625) do
     t.integer  "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
